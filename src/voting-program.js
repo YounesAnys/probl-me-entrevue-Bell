f7083a880d3e88279fb65734f213f2effa42fd4e
Younes Anys <yanys048@uottawa.ca>
2023-10-21 22:52:25 -0400

    fix of the logic of updating topCandidates

---
+++ src/voting-program.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+// Définir le chemin du fichier à partir duquel on va lire les données
+var filePath = 'votes.txt';
+// Set pour suivre les électeurs et détecter la fraude
+var voters = new Set();
+// Map pour suivre le nombre de votes pour chaque candidat
+var candidates = new Map();
+var topCandidates = [];
+// Créer un ReadStream pour lire les données à partir du fichier
+var readStream = fs.createReadStream(filePath, 'utf-8');
+readStream.on('data', function (data) {
+    // Diviser les données en lignes
+    var lines = data.split('\n');
+    lines.forEach(function (line) {
+        var match = line.match(/\(([^,]+), (\d+)\)/); // Utilisation d'une expression régulière pour extraire les données
+        if (match) {
+            var voterId = match[1];
+            var candidateId = parseInt(match[2], 10);
+            // Vérifier la fraude
+            if (voters.has(voterId)) {
+                console.log("Fraude d\u00E9tect\u00E9e: l'\u00E9lecteur ".concat(voterId, " a vot\u00E9 plus d'une fois."));
+            }
+            else {
+                voters.add(voterId);
+                if (!isNaN(candidateId)) {
+                    if (candidates.has(candidateId)) {
+                        candidates.set(candidateId, candidates.get(candidateId) + 1);
+                    }
+                    else {
+                        candidates.set(candidateId, 1);
+                    }
+                    // Mettre à jour du tableau topCandidates avec les 3 premiers candidats avec le plus de votes
+                    var sortedCandidates = Array.from(candidates.entries()).sort(function (a, b) { return b[1] - a[1]; });
+                    topCandidates.length = 0; // Réinitialiser le tableau
+                    for (var i = 0; i < 3 && i < sortedCandidates.length; i++) {
+                        topCandidates.push(sortedCandidates[i][0]);
+                    }
+                }
+                else {
+                    console.log("Erreur : Identifiant de candidat non valide - \"".concat(candidateId, "\""));
+                }
+            }
+        }
+    });
+});
+readStream.on('end', function () {
+    // Logique pour renvoyer les candidats
+    // Print les 3 premiers candidats
+    console.log('Les 3 premiers candidats :');
+    for (var _i = 0, topCandidates_1 = topCandidates; _i < topCandidates_1.length; _i++) {
+        var candidateId = topCandidates_1[_i];
+        console.log("Candidat ".concat(candidateId, " - Votes: ").concat(candidates.get(candidateId)));
+    }
+});
+readStream.on('error', function (error) {
+    console.error('Erreur de lecture du fichier txt :', error);
+});
--- src/voting-program.ts
+++ src/voting-program.ts
@@ -1,78 +1,72 @@
 import * as fs from 'fs';
 
-//// le  path du fichier à partir duquel on va lire les donnees
+// Définir le chemin du fichier à partir duquel on va lire les données
 const filePath = 'votes.txt';
 
-
-// Définir la structure des donnees d'un vote
+// Définir la structure des données d'un vote
 interface Vote {
-  voterId: string; // Id de l'electeur
+  voterId: string; // Id de l'électeur
   candidateId: number; // Id du candidat
 }
 
-// Set pour suivre les electeurs et detecter la fraude
+// Set pour suivre les électeurs et détecter la fraude
 const voters: Set<string> = new Set();
 
 // Map pour suivre le nombre de votes pour chaque candidat
 const candidates: Map<number, number> = new Map();
 
-
 const topCandidates: number[] = [];
 
-
-// Creer un ReadStream pour lire les données à partir du fichier
-// Exemple suivi pour reference : https://stackoverflow.com/questions/33643107/read-and-write-a-text-file-in-typescript
-
+// Créer un ReadStream pour lire les données à partir du fichier
 const readStream = fs.createReadStream(filePath, 'utf-8');
 
 readStream.on('data', (data: string) => {
-    // Diviser les donnees en lignes
+  // Diviser les données en lignes
   const lines = data.split('\n');
   lines.forEach((line) => {
-    if (line.trim() !== '') {
-        // Analyser l'Id de l'électeur et l'Id du candidat a partir de la ligne
-      const [voterId, candidateId] = line
-        .slice(1, -1)  // Supprimer les parentheses
-        .split(',')
-        .map((part) => part.trim());
-      const candidateIdNum = parseInt(candidateId, 10);
+    const match = line.match(/\(([^,]+), (\d+)\)/); // Utilisation d'une expression régulière pour extraire les données
 
+    if (match) {
+      const voterId = match[1];
+      const candidateId = parseInt(match[2], 10);
 
-    // Verifier si fraude
+      // Vérifier la fraude
       if (voters.has(voterId)) {
-        console.log(`Fraude detectee: l'electeur ${voterId} a voté plus d'une fois .`);
+        console.log(`Fraude détectée: l'électeur ${voterId} a voté plus d'une fois.`);
       } else {
         voters.add(voterId);
 
-        if (candidates.has(candidateIdNum)) {
-          candidates.set(candidateIdNum, candidates.get(candidateIdNum)! + 1);
-        } else {
-          candidates.set(candidateIdNum, 1);
-        }
+        if (!isNaN(candidateId)) {
+          if (candidates.has(candidateId)) {
+            candidates.set(candidateId, candidates.get(candidateId)! + 1);
+          } else {
+            candidates.set(candidateId, 1);
+          }
 
-         // Update du tableau topCandidates avec les 3 premiers candidats avec le plus de votes
-         topCandidates.sort((a, b) => candidates[b] - candidates[a]);
-         if (
-           topCandidates.length < 3 ||
-           candidates[candidateIdNum] >= candidates[topCandidates[2]]
-         ) {
-           topCandidates.push(candidateIdNum);
-           topCandidates.sort((a, b) => candidates[b] - candidates[a]);
-           if (topCandidates.length > 3) {
-             topCandidates.pop();
-           }
-         }
-       }
-     }
-   });
- });
+          // Mettre à jour du tableau topCandidates avec les 3 premiers candidats avec le plus de votes
+          const sortedCandidates = Array.from(candidates.entries()).sort(
+            (a, b) => b[1] - a[1]
+          );
+
+          topCandidates.length = 0; // Réinitialiser le tableau
+
+          for (let i = 0; i < 3 && i < sortedCandidates.length; i++) {
+            topCandidates.push(sortedCandidates[i][0]);
+          }
+        } else {
+          console.log(`Erreur : Identifiant de candidat non valide - "${candidateId}"`);
+        }
+      }
+    }
+  });
+});
 
 readStream.on('end', () => {
-  // logique pour renvoyer les candidats
-  // Print les 3 premeirs candidats
+  // Logique pour renvoyer les candidats
+  // Print les 3 premiers candidats
   console.log('Les 3 premiers candidats :');
   for (const candidateId of topCandidates) {
-    console.log(`Candidat ${candidateId} - Votes: ${candidates[candidateId]}`);
+    console.log(`Candidat ${candidateId} - Votes: ${candidates.get(candidateId)}`);
   }
 });
 
